47e81661ecaa4cb3a793e00564d90dc9df256f0c
JP Tiyasi <theanthem8@gmail.com>
2025-05-02 22:23:28 +0000

    Create waste type model

---
+++ src/main/java/com/enviro/assessment/inter001/johannestiyasi/model/WasteType.java
@@ -0,0 +1,25 @@
+package com.enviro.assessment.inter001.johannestiyasi.model;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class WasteType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Name is required")
+    private String name;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    private boolean recyclable;
+}

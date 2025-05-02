21fb956231570d49562933aa04e00ded2cee1ab6
JP Tiyasi <theanthem8@gmail.com>
2025-05-02 22:21:56 +0000

    Create user controller

---
+++ src/main/java/com/enviro/assessment/inter001/johannestiyasi/model/AppUser.java
@@ -0,0 +1,41 @@
+package com.enviro.assessment.inter001.johannestiyasi.model;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AppUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Username is required")
+    @Column(unique = true)
+    private String username;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
+    @Column(unique = true)
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role = UserRole.USER;
+
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    public enum UserRole {
+        USER, ADMIN
+    }
+}
